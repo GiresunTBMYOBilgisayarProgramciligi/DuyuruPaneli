1d25d46913a1bddc403e4be02a2c76f16c3ff994
sametatabasch <sametatabasch@gmail.com>
2020-07-27 13:06:31 +0300

    5 dakikada bir yenilenmesi sağlandı.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ setlocale(LC_ALL, 'tr_TR.UTF-8');
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta http-equiv="refresh" content="300">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
