24d1830877e0199fd925850f7d77107eaf48f5b9
sametatabasch <sametatabasch@gmail.com>
2020-07-29 14:36:56 +0300

    javascript dosyaları yerelde kullanılacak şekilde ayarlandı.

--- index.php
+++ index.php
@@ -154,12 +154,10 @@ setlocale(LC_ALL, 'tr_TR.UTF-8');
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
 <!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
-<script src="https://code.jquery.com/jquery-migrate-git.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
+<script src="js/jquery-migrate-git.min.js"></script>
+<script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/breaking-news-ticker.min.js"></script>
 <script type="text/javascript">
