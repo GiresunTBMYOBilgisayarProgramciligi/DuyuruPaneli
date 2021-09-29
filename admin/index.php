97bd097839aaf23552339c5e04edeaafafad33dd
sametatabasch <sametatabasch@gmail.com>
2021-09-29 14:51:05 +0300

    Kullanılmayan bazı javascript dosyaları kaldırıldı

--- admin/index.php
+++ admin/index.php
@@ -134,9 +134,7 @@ $slides = new SlideController();
 <script src="vendors/js/vendor.bundle.base.js"></script>
 <!-- endinject -->
 <!-- Plugin js for this page -->
-<script src="vendors/chart.js/Chart.min.js"></script>
-<script src="vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-<script src="vendors/progressbar.js/progressbar.min.js"></script>
+
 
 <!-- End plugin js for this page -->
 <!-- inject:js -->
@@ -144,13 +142,9 @@ $slides = new SlideController();
 <script src="js/hoverable-collapse.js"></script>
 <script src="js/template.js"></script>
 <script src="js/settings.js"></script>
-<script src="js/todolist.js"></script>
-<script src="vendors/pwstabs/jquery.pwstabs.min.js"></script>
-<script src="js/tabs.js"></script>
 <!-- endinject -->
 <!-- Custom js for this page-->
 <script src="js/dashboard.js"></script>
-<script src="js/Chart.roundedBarCharts.js"></script>
 <!-- End custom js for this page-->
 </body>
 
