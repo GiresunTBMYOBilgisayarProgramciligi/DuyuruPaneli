7d4a1f030e9b618e72ba6b3004800845aa922838
sametatabasch <sametatabasch@gmail.com>
2021-11-21 21:48:49 +0300

    yeni mpm kullanımına göre giriş sayfası güncellendi

--- admin/loginView.php
+++ admin/loginView.php
@@ -18,22 +18,20 @@ if ($users->isLoggedIn()) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Star Admin2 </title>
+    <title>Tirebolu MBMYO Duyuru Paneli </title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/feather/feather.css">
-    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="vendors/typicons/typicons.css">
-    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
     <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="node_modules/dropify/dist/css/dropify.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="css/vertical-layout-light/style.css">
     <!-- endinject -->
     <link rel="shortcut icon" href="images/favicon.png"/>
-    <script src="vendors/js/vendor.bundle.base.js"></script>
+
+    <link rel="stylesheet" href="css/custom.css">
 </head>
 
 <body>
@@ -81,6 +79,7 @@ if ($users->isLoggedIn()) {
     <!-- page-body-wrapper ends -->
 </div>
 <!-- container-scroller -->
+<script src="js/bundle.js"></script>
 <script>
     $('#loginForm').submit(function(event){
         event.preventDefault();
@@ -109,19 +108,9 @@ if ($users->isLoggedIn()) {
     })
 
 </script>
-<!-- plugins:js -->
 
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<script src="vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="js/off-canvas.js"></script>
-<script src="js/hoverable-collapse.js"></script>
-<script src="js/template.js"></script>
-<script src="js/settings.js"></script>
-<script src="js/todolist.js"></script>
-<!-- endinject -->
+
+
 </body>
 
 </html>
